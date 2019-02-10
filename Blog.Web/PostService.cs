0028c6b7d69364b2cb1cd70beddc3d6b906209fc
Stoyan <slownessbg@gmail.com>
2019-02-10 22:41:19 +0200

    Refactoring the main service class

---
+++ Blog.Web/PostService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Web.DAL;
+using Npgsql;
+
+namespace Blog.Web
+{
+    public class PostService
+    {
+        private Database Database { get; set; }
+
+        public PostService(Database database)
+        {
+            this.Database = database;
+        }
+
+        public PostPoco GetPostById(int id)
+        {
+            var post = this.Database.QueryOne<PostPoco>("SELECT * FROM posts WHERE post_id=@i;", new NpgsqlParameter("i", id));
+
+            return post;
+        }
+    }
+}
