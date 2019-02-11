1ba1fb3917b7eeadd56872240c1615821b35a138
Stoyan <slownessbg@gmail.com>
2019-02-11 22:09:35 +0200

    Refactoring to more services

--- Blog.Web/PostService.cs
+++ Blog.Web/PostService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Blog.Web.DAL;
+using Blog.Web.Models;
 using Npgsql;
 
 namespace Blog.Web
@@ -21,6 +22,31 @@ namespace Blog.Web
             var post = this.Database.QueryOne<PostPoco>("SELECT * FROM posts WHERE post_id=@i;", new NpgsqlParameter("i", id));
 
             return post;
+        }
+
+        
+
+        public List<LightPostModel> GetLatest(int count)
+        {
+            var posts = this.Database.Query<PostPoco>("SELECT * FROM posts ORDER BY post_id LIMIT @l;", new NpgsqlParameter("l", count));
+            var finishedModels = new List<LightPostModel>();
+            foreach (var postPoco in posts)
+            {
+                var lightPostModel = new LightPostModel
+                {
+                    Content = string.Join(" ", postPoco.Content.Split(' ').Take(8)),
+                    PostId = postPoco.PostId,
+                    Title = postPoco.Title
+                };
+
+                //var tags = this.GetTags(postPoco.PostId);
+
+                //lightPostModel.Tags = tags.Select(tagPoco => tagPoco.Name).ToArray();
+
+                finishedModels.Add(lightPostModel);
+            }
+
+            return finishedModels;
         }
     }
 }
