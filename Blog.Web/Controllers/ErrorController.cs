7966dc4594fd4c144289b00947e94136cbb6ade5
Stoyan <slownessbg@gmail.com>
2019-04-17 17:26:31 +0300

    adding not found page

---
+++ Blog.Web/Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}
