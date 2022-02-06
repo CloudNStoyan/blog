5c8ff244f02e8ef63d2af859093a1b89726b2fd4
Stoyan <slownessbg@gmail.com>
2022-02-07 00:04:02 +0200

    adding mobile menu

---
+++ Blog.Web/wwwroot/js/mobile-menu.js
@@ -0,0 +1,14 @@
+﻿const menuBtn = document.querySelector('.menu');
+const navMenu = document.querySelector('.navigation nav');
+
+const toggleMenu = (e) => {
+    e.preventDefault();
+
+    menuBtn.classList.toggle('hide');
+    navMenu.classList.toggle('show');
+}
+
+if (menuBtn) {
+    menuBtn.addEventListener('click', toggleMenu);
+}
+
