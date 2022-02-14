4f8ce2e20a6c14ba06ae682995751192dfdad170
Stoyan <slownessbg@gmail.com>
2022-02-14 23:29:41 +0200

    restructuring and adding better ui for adding tags to posts

---
+++ Blog.Web/wwwroot/admin/js/post/createoredit.js
@@ -0,0 +1,48 @@
+﻿const tagBackendInput = document.querySelector('#Tags');
+const tagInput = document.querySelector('.tag-input');
+const tagsContainer = document.querySelector('.tags');
+
+let tags = [...tagBackendInput.value.split(',')];
+
+const removeTag = (tagName, tagEl) => {
+    tagEl.remove();
+    tags = tags.filter(tag => tag !== tagName);
+    tagBackendInput.value = tags.join(',');
+}
+
+tagsContainer.querySelectorAll('.tag')
+    .forEach(tagEl => tagEl.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        removeTag(tagEl.innerText, tagEl);
+    }));
+
+tagInput.addEventListener('keydown', (e) => {
+    if (e.code !== 'Comma') {
+        return;
+    }
+
+    e.preventDefault();
+
+    const tagName = tagInput.value.trim();
+
+    tagInput.value = '';
+
+    if (!tagName || tagName.length === 0 || tags.includes(tagName)) {
+        return;
+    }
+    
+    const tag = document.createElement('span');
+    tag.innerText = tagName;
+    tag.className = 'tag';
+    tag.addEventListener('click', (tagE) => {
+        tagE.preventDefault();
+        removeTag(tagName, tag);
+    });
+
+    tagsContainer.insertBefore(tag, tagsContainer.lastElementChild);
+
+    tags.push(tagName);
+
+    tagBackendInput.value = tags.join(',');
+});
