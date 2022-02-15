53343b226b8c92d058d4e8f9e0b1bc61d7362e26
Stoyan <slownessbg@gmail.com>
2022-02-15 14:12:45 +0200

    better UX with animations

--- Blog.Web/wwwroot/admin/js/post/createoredit.js
+++ Blog.Web/wwwroot/admin/js/post/createoredit.js
@@ -4,8 +4,24 @@ const tagsContainer = document.querySelector('.tags');
 
 let tags = [...tagBackendInput.value.split(',')];
 
+const faAnimations = {
+    fade: 'fa-fade',
+    beat: 'fa-beat',
+    shake: 'fa-shake'
+}
+
+const animate = (el, animName, onEnd, duration = 500) => {
+    el.classList.add(animName);
+    setTimeout(() => {
+        el.classList.remove(animName);
+        if (typeof onEnd === 'function') {
+            onEnd();
+        }
+    }, duration);
+}
+
 const removeTag = (tagName, tagEl) => {
-    tagEl.remove();
+    animate(tagEl, faAnimations.fade, () => tagEl.remove());
     tags = tags.filter(tag => tag !== tagName);
     tagBackendInput.value = tags.join(',');
 }
@@ -28,7 +44,17 @@ tagInput.addEventListener('keydown', (e) => {
 
     tagInput.value = '';
 
-    if (!tagName || tagName.length === 0 || tags.includes(tagName)) {
+    if (!tagName || tagName.length === 0) {
+        return;
+    }
+
+    if (tags.includes(tagName)) {
+        const tagElements = Array.from(tagsContainer.children);
+
+        const tagEl = tagElements.find(el => el.innerText.trim() === tagName);
+
+        animate(tagEl, faAnimations.shake);
+
         return;
     }
     
@@ -42,6 +68,8 @@ tagInput.addEventListener('keydown', (e) => {
     });
 
     tagsContainer.insertBefore(tag, tagsContainer.lastElementChild);
+
+    animate(tag, faAnimations.beat, null, 150);
 
     tags.push(tagName);
 
