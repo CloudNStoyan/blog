18c65ae68cf22cfc6b5782cc206c1fc5f8d772ea
Stoyan <slownessbg@gmail.com>
2022-03-01 23:53:54 +0200

    adding toasts

---
+++ Blog.Web/wwwroot/js/toast.js
@@ -0,0 +1,12 @@
+﻿const toastContainer = document.querySelector('.toast-container');
+
+const createToast = (toastText) => {
+    const toast = document.createElement('div');
+    toast.className = 'toast';
+    toast.innerText = toastText;
+    setTimeout(() => {
+        toast.classList.add('remove');
+        setTimeout(() => toast.remove(), 1500);
+    }, 2000);
+    toastContainer.appendChild(toast);
+}
