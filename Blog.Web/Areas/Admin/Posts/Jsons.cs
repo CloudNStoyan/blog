958eb1a522d75bc90e6b28858a660f4940a6451a
Stoyan <slownessbg@gmail.com>
2022-04-09 18:38:27 +0300

    refactoring

---
+++ Blog.Web/Areas/Admin/Posts/Jsons.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace Blog.Web.Areas.Admin.Posts;
+
+public class PostJson
+{
+    [JsonProperty("post_id")]
+    public int PostId { get; set; }
+}
+
+public class UserJson
+{
+    [JsonProperty("avatar")]
+    public string Avatar { get; set; }
+
+    [JsonProperty("username")]
+    public string Username { get; set; }
+}
---
+++ Blog.Web/wwwroot/js/markdown-addon.js
@@ -0,0 +1,31 @@
+﻿const content = document.querySelector('.content');
+const headings = content.querySelectorAll('h1,h2,h3,h4,h5,h6');
+
+const addAnchor = (el) => {
+    const anchor = document.createElement('a');
+    anchor.href = `#${el.id}`;
+    anchor.innerHTML = '<i class="fa-solid fa-link"></i>';
+    anchor.className = 'heading-anchor';
+    el.insertBefore(anchor, el.firstChild);
+}
+
+headings.forEach(addAnchor);
+
+const codeWrappers = content.querySelectorAll('pre code');
+
+const addCopyToClipboard = (el) => {
+    const copyBtn = document.createElement('a');
+    copyBtn.href = '#';
+    copyBtn.addEventListener('click',
+        (e) => {
+            e.preventDefault();
+
+            navigator.clipboard.writeText(el.innerText.trim());
+            createToast('Copied to clipboard.');
+        });
+    copyBtn.className = 'copy-btn';
+    copyBtn.innerText = 'Copy';
+    el.parentElement.insertBefore(copyBtn, el);
+}
+
+codeWrappers.forEach(addCopyToClipboard);
