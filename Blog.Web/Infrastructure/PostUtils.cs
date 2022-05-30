a033190b19e38cd8971c81fd21afd00866977b02
Stoyan Kolev <slownessbg@gmail.com>
2022-05-30 23:50:32 +0300

    extract excerp code as its own method

---
+++ Blog.Web/Infrastructure/PostUtils.cs
@@ -0,0 +1,7 @@
+﻿namespace Blog.Web.Infrastructure
+{
+    public static class PostUtils
+    {
+        public static string GetExcerp(string content, int maxLength) => content.Length > maxLength ? $"{content[..maxLength]}.." : content;
+    }
+}
